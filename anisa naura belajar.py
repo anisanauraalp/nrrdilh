987808805b0ef5018e52e2e7444d0f14eb528e58
anisa naura adilah <anisanauraalp@gmail.com>
2026-03-13 09:14:31 +0700

    Add files via upload

---
+++ anisa naura belajar.py
@@ -0,0 +1,12 @@
+# Program Python Sederhana
+
+nama = input("Masukkan nama kamu: ")
+umur = int(input("Masukkan umur kamu: "))
+
+print("\nHalo", nama)
+print("Umur kamu", umur, "tahun")
+
+if umur >= 18:
+    print("Kamu sudah dewasa.")
+else:
+    print("Kamu masih di bawah umur.")
